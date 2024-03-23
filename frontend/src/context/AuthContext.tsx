3c76d111192ad2648c935470f4308a8ebee7b62b
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-03-24 01:59:00 +0530

    + AuthContext

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,46 @@
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type User = {
+  name: string;
+  email: string;
+};
+
+type UserAuth = {
+  isLoggedIn: boolean;
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  singup: (name: string, email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+};
+
+const AuthContext = createContext<UserAuth | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // fetct if the user's cookies are valid then skip login page
+  }, []);
+
+  const login = async (email: string, password: string) => {};
+  const signup = async (name: string, email: string, password: string) => {};
+  const logout = async () => {};
+
+  const value = {
+    user,
+    isLoggedIn,
+    login,
+    logout,
+    signup,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+export const useAuth = () => useContext(AuthContext);

d399a4e9065cb11b6e74ffdc51c648ba4169fa8f
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-04-19 19:11:55 +0530

    Fixing for producation

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,20 +1,19 @@
 import express from "express";
 import { config } from "dotenv";
-import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 // Load environment variables from .env file
 config();
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://lazarus-gpt.vercel.app", credentials: true, methods: ["GET", "POST"] }));
 // Parse incoming JSON requests
 app.use(express.json());
 // Parse incoming Cookie header and populate req.cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
 // Log requests to the console
 // Remove this middleware for production
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use("/api/v1", appRouter);
 export default app;
 //# sourceMappingURL=app.js.map

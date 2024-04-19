bb2863b2bae48e53298608e2725fcf28de3ca85e
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-04-19 19:37:45 +0530

    Fixing for producation

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -6,7 +6,7 @@ import cors from "cors";
 // Load environment variables from .env file
 config();
 const app = express();
-app.use(cors({ origin: "https://lazarus-gpt.vercel.app", credentials: true, methods: ["GET", "POST"] }));
+app.use(cors({ origin: "https://lazarus-gpt.vercel.app/", credentials: true, methods: ["GET", "POST"] }));
 // Parse incoming JSON requests
 app.use(express.json());
 // Parse incoming Cookie header and populate req.cookies
